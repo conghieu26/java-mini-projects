97b0f0499a47afb90bb4102194f38eb22c5f84bf
Nguyen Hieu <hieunc00@gmail.com>
2025-11-04 12:10:02 +0700

    Add Object Customer

---
+++ Collections/src/ex/Customer.java
@@ -0,0 +1,18 @@
+package ex;
+
+public class Customer {
+	private String id;
+    private String name;
+	public String getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	@Override
+	public String toString() {
+		return id + " - " + name;
+	}
+    
+    
+}
