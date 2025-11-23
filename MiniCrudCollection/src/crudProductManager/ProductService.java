220493c01b203dfa0dbfa8f65392bdb127c8f8fc
Nguyen Hieu <hieunc00@gmail.com>
2025-11-23 16:19:54 +0700

    feat: add ProductService CRUD (add, edit, delete, find, list) with validation and exception handling

--- MiniCrudCollection/src/crudProductManager/ProductService.java
+++ MiniCrudCollection/src/crudProductManager/ProductService.java
@@ -1,6 +1,7 @@
 package crudProductManager;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ProductService {
@@ -12,12 +13,60 @@ public class ProductService {
 	public void AddProduct(Product p) {
 		boolean exits = products.stream().anyMatch(product -> product.getId() == p.getId());
 		
-		if(!exits) {
+		if(exits) {
 			throw new InvalidProductException("Product Id already exist!!");
+		}
+		
+		if (p.getPrice() <= 0) {
+			throw new InvalidProductException("Price must be greater than 0!");
 		}
 		
 		products.add(p);
 		System.out.println("Product already added!!");
 	}
+	
+	//-----------------Delete Product-----------------
+	
+	public void DeleteProduct(int id) {
+		
+		 boolean removed = products.removeIf(product -> product.getId() == id);
+		 
+		 if(!removed) {
+			 throw new ProductNotFoundException("Not found product id");
+		 }
+		 
+		 System.out.println("Product deleted!");
+	}
+	
+	//-----------------Edit Product-----------------
+	
+	public void EditProduct(int id, String newName, double newPrice, Category newCategory) {
+		
+		Product editProduct = products.stream().filter(product -> product.getId() == id).findFirst()
+										 .orElseThrow(() -> new InvalidProductException("Cannot edit: Product with id " + id + " not found!"));
+		
+		if (newPrice <= 0) {
+	        throw new InvalidProductException("Price must be greater than 0!");
+	    }
+		
+		editProduct.setName(newName);
+		editProduct.setPrice(newPrice);
+		editProduct.setCategory(newCategory);
+		
+		System.out.println("Product updated!");
+	}
+	
+	//-----------------Find Product-----------------
+	
+	public List<Product> findProduct(Integer id, Category category, Double maxPrice) {
+		return products.stream().filter(p -> (id != null && p.getId() == id) || (category != null && p.getCategory() == category) || (maxPrice != null && p.getPrice() < maxPrice)).toList();
+	}
+	
+	//-----------------Print Product-----------------
+	
+	public List<Product> printProduct() {
+		return products.stream().sorted(Comparator.comparing(Product::getPrice)).toList();
+	}
+	
 	
 }
